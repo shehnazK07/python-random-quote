33d41e91889cc0c2c94697560bbdc6c414acc258
Shehnaz Khan <ShehnazK@citiustech.com>
2020-07-01 12:23:41 +0530

    Explored python by itself

--- get-quote.py
+++ get-quote.py
@@ -4,12 +4,15 @@ def main():
 
   f = open("quotes.txt")
   quotes = f.readlines()
-  f.close()
+  f.close() 
 
-  last = 13
-  #last = len(quotes) - 1
-  rnd = random.randint(0, last)
-  print(quotes[rnd])
+  #last = 13
+  last = len(quotes) - 1
+  #for i in range(0,last) 
+  #rnd = random.randint(0, last)
+  print (quotes[random.randint(0, last)])
+  print (quotes[random.randint(1, last)])
+    #print (quotes[rnd]) 
 
 if __name__== "__main__":
   main()
